97689ff02992e6bbbb043f24b8523468ce64160f
Kelvin <thanhsang94@gmail.com>
2018-10-10 17:15:27 +0700

    change socketio

---
+++ routes/socketio.js
@@ -0,0 +1,35 @@
+exports.foo = function(req,res){
+    // now use socket.io in your routes file
+    var io = req.app.get('socketio');
+
+    //Configure GPIO PIN
+    const Gpio = require('onoff').Gpio;
+    ac1 = new Gpio(4, 'out');
+    ac2 = new Gpio(17, 'out');
+    ac3 = new Gpio(27, 'out');
+
+    //Open connection socket
+    io.sockets.on('connection', function (socket) { // WebSocket Connection
+        //Param
+        var p1Value = 0; //static variable for current status
+        var p2Value = 0; //static variable for current status
+
+        socket.on('pump1', function (data) { //get light switch status from client
+            p1Value = data;
+            console.log('PUMP1: ' + p1Value);
+            if (p1Value != ac1.readSync()) {
+                ac1.writeSync(p1Value);
+                //   console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+            }
+        });
+
+        socket.on('pump2', function (data) { //get light switch status from client
+            p2Value = data;
+            console.log('PUMP2: ' + p2Value);
+            if (p2Value != ac2.readSync()) {
+                ac2.writeSync(p2Value);
+                //   console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+            }
+        });
+    });
+}
--- server.js
+++ server.js
@@ -7,13 +7,7 @@ var express = require('express'),
   io = require('socket.io').listen(server),
   config = require('./config/config'),
   mongoose =require('mongoose');
-
-
-//Configure GPIO PIN
-Gpio = require('onoff').Gpio;
-ac1 = new Gpio(4, 'out');
-ac2 = new Gpio(17, 'out');
-ac3 = new Gpio(27, 'out');
+  app.set('socketio', io);
 
 //Connect to database
 mongoose.connect(config.database, {useMongoClient: true});
@@ -37,31 +31,6 @@ server.listen(config.port,()=>{
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
-});
-
-//Open connection socket
-io.sockets.on('connection', function (socket) { // WebSocket Connection
-  //Param
-  var p1Value = 0; //static variable for current status
-  var p2Value = 0; //static variable for current status
-
-  socket.on('pump1', function (data) { //get light switch status from client
-    p1Value = data;
-    console.log('PUMP1: ' + p1Value);
-    if (p1Value != ac1.readSync()) {
-      ac1.writeSync(p1Value);
-      //   console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
-    }
-  });
-
-  socket.on('pump2', function (data) { //get light switch status from client
-    p2Value = data;
-    console.log('PUMP2: ' + p2Value);
-    if (p2Value != ac2.readSync()) {
-      ac2.writeSync(p2Value);
-      //   console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
-    }
-  });
 });
 
 process.on('SIGINT', function () {

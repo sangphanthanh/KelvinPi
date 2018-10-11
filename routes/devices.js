17a61658175477f6e7bd7b364236065e53fc3206
Kelvin <thanhsang94@gmail.com>
2018-10-11 15:18:08 +0700

    fix bug

--- routes/devices.js
+++ routes/devices.js
@@ -4,10 +4,10 @@ const Device = require('../models/device');
 module.exports.pumpStatus = function(name,value){
     if(value == 1){
         console.log('value:1 ' + name);
-        device.updateDeviceStatus(name,true);
+        Device.updateDeviceStatus(name,true);
     }else{
         console.log('value:0 ' + name);
-        device.updateDeviceStatus(name,false);
+        Device.updateDeviceStatus(name,false);
     }
 }
 

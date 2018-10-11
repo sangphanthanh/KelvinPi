d49da8fbde11d691ea21467999e40791fc1b8b95
Kelvin <thanhsang94@gmail.com>
2018-10-11 10:49:31 +0700

    server fix

--- server.js
+++ server.js
@@ -6,25 +6,30 @@ var express = require('express'),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server),
   config = require('./config/config'),
-  mongoose =require('mongoose');
-  
+  mongoose = require('mongoose');
 
+const Gpio = require('onoff').Gpio;
+var ac1 = new Gpio(4, 'out');
+var ac2 = new Gpio(17, 'out');
+var ac3 = new Gpio(27, 'out');
 //Connect to database
-mongoose.connect(config.database, {useMongoClient: true});
+mongoose.connect(config.database, {
+  useMongoClient: true
+});
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 //On connection
-mongoose.connection.on('connected',()=>{
-	console.log('connected to database ' + config.database)
+mongoose.connection.on('connected', () => {
+  console.log('connected to database ' + config.database)
 });
 //On error
-mongoose.connection.on('error',(err)=>{
-	console.log('database error ' + err)
+mongoose.connection.on('error', (err) => {
+  console.log('database error ' + err)
 });
 
 //Configure Server 
-server.listen(config.port,()=>{
-  console.log('Server starting at port '+ config.port);
+server.listen(config.port, () => {
+  console.log('Server starting at port ' + config.port);
 });
 
 //Configure module
@@ -33,7 +38,33 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
 
-app.set('socketio', io);
+// app.set('socketio', io);
+
+
+//Open connection socket
+io.sockets.on('connection', function (socket) { // WebSocket Connection
+  //Param
+  var p1Value = 0; //static variable for current status
+  var p2Value = 0; //static variable for current status
+
+  socket.on('pump1', function (data) { //get light switch status from client
+    p1Value = data;
+    console.log('PUMP1: ' + p1Value);
+    if (p1Value != ac1.readSync()) {
+      ac1.writeSync(p1Value);
+      //   console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+    }
+  });
+
+  socket.on('pump2', function (data) { //get light switch status from client
+    p2Value = data;
+    console.log('PUMP2: ' + p2Value);
+    if (p2Value != ac2.readSync()) {
+      ac2.writeSync(p2Value);
+      //   console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+    }
+  });
+});
 
 process.on('SIGINT', function () {
   ac1.writeSync(0);
